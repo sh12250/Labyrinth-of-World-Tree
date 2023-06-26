ad075545798795edd42da5a8544a080899291180
sh12250 <99242620+sh12250@users.noreply.github.com>
2023-06-26 21:28:29 +0900

    Feat. 직업에 맞춰 출력하는 기능 추가

--- LabyrinthOfWorldTree/Scenes.cs
+++ LabyrinthOfWorldTree/Scenes.cs
@@ -59,7 +59,68 @@ namespace LabyrinthOfWorldTree
             Jobs value = new Jobs();
             int curJodID = 0;
 
-            switch (PickedJobID)
+            PrintCharacter(curJodID);
+
+            while (true)
+            {
+                ConsoleKeyInfo input = Console.ReadKey();
+
+                switch (input.KeyChar)
+                {
+                    case 'a':
+                    case 'A':
+                    case 'ㅁ':
+                        curJodID -= 1;
+
+                        if (curJodID == -1)
+                        {
+                            curJodID = 2;
+                        }
+
+                        Console.WriteLine(value.JobNames[curJodID]);
+
+                        break;
+                    case 'd':
+                    case 'D':
+                    case 'ㅇ':
+                        curJodID += 1;
+
+                        if (curJodID == 3)
+                        {
+                            curJodID = 0;
+                        }
+
+                        Console.WriteLine(value.JobNames[curJodID]);
+
+                        break;
+                    case 'e':
+                    case 'E':
+                    case 'ㄷ':
+                        PickedJobID = curJodID;
+
+                        Console.WriteLine("{0}를 선택하셨습니다", value.JobNames[PickedJobID]);
+
+                        break;
+                    default:
+                        Console.WriteLine("이상한 키 입력");
+
+                        break;
+                }
+
+                PrintCharacter(curJodID);
+
+                if (input.KeyChar == 'e' || input.KeyChar == 'E' || input.KeyChar == 'ㄷ')
+                {
+                    Console.WriteLine("{0}를 선택하셨습니다", value.JobNames[PickedJobID]);
+
+                    break;
+                }
+            }           // while()
+        }
+
+        public void PrintCharacter(int jobID)
+        {
+            switch (jobID)
             {
                 case 0:
                     Console.SetCursorPosition(0, 0);
@@ -189,189 +250,6 @@ namespace LabyrinthOfWorldTree
                     break;
             }
 
-            while (true)
-            {
-                ConsoleKeyInfo input = Console.ReadKey();
-
-                switch (input.KeyChar)
-                {
-                    case 'a':
-                    case 'A':
-                    case 'ㅁ':
-                        curJodID -= 1;
-
-                        if (curJodID == -1)
-                        {
-                            curJodID = 2;
-                        }
-
-                        Console.WriteLine(value.JobNames[curJodID]);
-
-                        break;
-                    case 'd':
-                    case 'D':
-                    case 'ㅇ':
-                        curJodID += 1;
-
-                        if (curJodID == 3)
-                        {
-                            curJodID = 0;
-                        }
-
-                        Console.WriteLine(value.JobNames[curJodID]);
-
-                        break;
-                    case 'e':
-                    case 'E':
-                    case 'ㄷ':
-                        PickedJobID = curJodID;
-
-                        Console.WriteLine("{0}를 선택하셨습니다", value.JobNames[PickedJobID]);
-
-                        break;
-                    default:
-                        Console.WriteLine("이상한 키 입력");
-
-                        break;
-                }
-
-                switch (curJodID)
-                {
-                    case 0:
-                        Console.SetCursorPosition(0, 0);
-
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-
-                        Console.WriteLine("　　　　　　　　　　　　　　기　사　　　　　　　　　　　　　　");
-
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-
-                        break;
-                    case 1:
-                        Console.SetCursorPosition(0, 0);
-
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-
-                        Console.WriteLine("　　　　　　　　　　　　　　해　적　　　　　　　　　　　　　　");
-
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-
-                        break;
-                    case 2:
-                        Console.SetCursorPosition(0, 0);
-
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-
-                        Console.WriteLine("　　　　　　　　　　　　점　성　술　사　　　　　　　　　　　　");
-
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-                        Console.WriteLine("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");
-
-                        break;
-                }
-
-                if (input.KeyChar == 'e' || input.KeyChar == 'E' || input.KeyChar == 'ㄷ')
-                {
-                    Console.WriteLine("{0}를 선택하셨습니다", value.JobNames[PickedJobID]);
-
-                    break;
-                }
-            }           // while()
         }
     }
 }
