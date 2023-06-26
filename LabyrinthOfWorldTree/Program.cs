0a1cb7216d662ee18164a1188e13e194ae97aa6c
sh12250 <99242620+sh12250@users.noreply.github.com>
2023-06-26 13:16:30 +0900

    Create. 프로그램

---
+++ LabyrinthOfWorldTree/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabyrinthOfWorldTree
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(125, 40);
+
+            TitleScene scene = new TitleScene();
+
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
