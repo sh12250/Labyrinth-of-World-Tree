9ed5d0a8c4533d421de49b3e5136f18d0e0d54a7
sh12250 <99242620+sh12250@users.noreply.github.com>
2023-06-26 13:54:48 +0900

    Fix. 직업 클래스의 멤버 변수들을 생성자로 초기화

--- LabyrinthOfWorldTree/Jobs.cs
+++ LabyrinthOfWorldTree/Jobs.cs
@@ -8,13 +8,24 @@ namespace LabyrinthOfWorldTree
 {
     public class Jobs
     {
-        public string[] jobNames = new string[] { "기사", "해적", "점성술사" };
-        // 레벨업마다 스탯이 올라갈 확률
-        // 각 300씩 설정 스킬을 추가하면 다시 설정할 필요가 있다
-        public int[] strUpRate = new int[] { 80, 60, 10 };
-        public int[] tecUpRate = new int[] { 60, 80, 80 };
-        public int[] vitUpRate = new int[] { 80, 60, 40 };
-        public int[] wisUpRate = new int[] { 40, 50, 80 };
-        public int[] lucUpRate = new int[] { 40, 80, 80 };
+        public string[] JobNames { get; private set; }
+        public int[] StrUpRate { get; private set; }
+        public int[] TecUpRate { get; private set; }
+        public int[] VitUpRate { get; private set; }
+        public int[] WisUpRate { get; private set; }
+        public int[] LucUpRate { get; private set; }
+
+        public Jobs()
+        {
+            JobNames = new string[] { "기사", "해적", "점성술사" };
+
+            // 레벨업마다 스탯이 올라갈 확률
+            // 각 300씩 설정 스킬을 추가하면 다시 설정할 필요가 있다
+            StrUpRate = new int[] { 80, 60, 40 };
+            TecUpRate = new int[] { 40, 80, 80 };
+            VitUpRate = new int[] { 80, 40, 40 };
+            WisUpRate = new int[] { 60, 40, 80 };
+            LucUpRate = new int[] { 40, 80, 60 };
+        }
     }
 }
